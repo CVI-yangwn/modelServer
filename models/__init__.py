45d8624e5f834b28f529689c5674dc16a65efece
CVI-yangwn <2295311202@qq.com>
2025-09-25 17:18:04 +0800

    fix model init

--- models/__init__.py
+++ models/__init__.py
@@ -14,18 +14,19 @@ from .qwen2_5 import Qwen2_5_VL
 from .qwen3 import Qwen3
 
 class LazyModel:
-    def __init__(self, model_class:ModelBase, weight_path):
+    def __init__(self, model_class:ModelBase, weight_path, **kwargs):
         self._instance = None
         self.model_class = model_class
         self.weight_path = weight_path
+        self.kwargs = kwargs
 
     def get_instance(self):
         if self._instance is None:
             print(f"Creating {self.model_class} instance...")
-            self._instance = self.model_class(self.weight_path)
+            self._instance = self.model_class(self.weight_path, **self.kwargs)
         return self._instance
     
 Qwen2_5_VL_3B = LazyModel(Qwen2_5_VL, os.path.join(_current_dir, 'weights', 'Qwen2.5-VL-3B'))
 Qwen2_5_VL_7B = LazyModel(Qwen2_5_VL, os.path.join(_current_dir, 'weights', 'Qwen2.5-VL-7B'))
 Qwen2_5_VL_32B = LazyModel(Qwen2_5_VL, os.path.join(_current_dir, 'weights', 'Qwen2.5-VL-32B'))
-Qwen3_14B = LazyModel(Qwen3, os.path.join(_current_dir, 'weights', 'Qwen3-14B'))
+Qwen3_14B = LazyModel(Qwen3, os.path.join(_current_dir, 'weights', 'Qwen3-14B'), enable_thinking=True)
