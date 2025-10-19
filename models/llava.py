91d90fdd554d9c44e404e9ae27bf5ff49fbb172e
CVI-yangwn <2295311202@qq.com>
2025-10-19 18:50:45 +0800

    add llava-med

---
+++ models/llava.py
@@ -0,0 +1,82 @@
+"""
+pip install git+https://github.com/microsoft/LLaVA-Med.git
+"""
+import torch
+from .model_base import ModelBase
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import argparse
+import torch
+import os
+import json
+from tqdm import tqdm
+import shortuuid
+
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria, process_images
+
+from PIL import Image
+import math
+from transformers import set_seed, logging
+
+logging.set_verbosity_error()
+
+
+def split_list(lst, n):
+    """Split a list into n (roughly) equal-sized chunks"""
+    chunk_size = math.ceil(len(lst) / n)  # integer division
+    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
+
+
+def get_chunk(lst, n, k):
+    chunks = split_list(lst, n)
+    return chunks[k]
+
+
+class Llava_med_1_5(ModelBase):
+    def __init__(self, model_path):
+        super().__init__(model_path)
+
+    def _load_model(self):
+        set_seed(0)
+        # Model
+        disable_torch_init()
+        model_path = os.path.expanduser(self.model_path)
+        model_name = get_model_name_from_path(model_path)
+        self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model(model_path, None, model_name)
+
+    def ask_with_images(self, question:str, images:list, history=[]):
+        qs = question.replace(DEFAULT_IMAGE_TOKEN, '').strip()
+        if self.model.config.mm_use_im_start_end:
+            qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+        else:
+            qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+
+        conv = conv_templates['vicuna_v1'].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
+        input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+
+        # image = Image.open(images[0])
+        image_tensor = process_images([Image.open(img_path) for img_path in images], self.image_processor, self.model.config)[0]
+
+        with torch.inference_mode():
+            output_ids = self.model.generate(
+                input_ids,
+                images=image_tensor.unsqueeze(0).half().cuda(),
+                do_sample=False,
+                temperature=0,
+                top_p=None,
+                num_beams=1,
+                # no_repeat_ngram_size=3,
+                max_new_tokens=1024,
+                use_cache=True)
+
+        outputs = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+
+        return outputs
